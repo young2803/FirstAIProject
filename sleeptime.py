f6958f50158bfc130dd447c0cdd6a4ba500a9786
Firebear518 <phj040518@gmail.com>
2025-02-20 16:56:21 +0900

    Delete SleepTime >=12 <=3

--- sleeptime.py
+++ sleeptime.py
@@ -5,12 +5,11 @@ import seaborn as sns
 
 df = pd.read_csv('datasets/sleeptime_prediction_dataset.csv')
 df = df.drop('ReadingTime', axis=1)
+df = df[(df['SleepTime'] <= 10) & (df['SleepTime'] >= 3)]
 
 feature = ['WorkoutTime', 'PhoneTime', 'WorkHours', 'CaffeineIntake', 'RelaxationTime']
 label = ['SleepTime']
 
-corr_matrix = df.corr()
-
-plt.title('Correlation Matrix')
-sns.heatmap(corr_matrix, annot=True)
+print(df.info())
+sns.boxplot(y=df['SleepTime'])
 plt.show()
