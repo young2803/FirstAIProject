eae13e0d31774e6765aeb1c7a312a42bb1f0ac32
young <c0s3young@gmail.com>
2025-02-24 16:36:59 +0900

    mlp1

---
+++ obesity/upgrade_ver_model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+import pandas as pd
+
+# 데이터 불러오기 (전처리 담당)
+X_train = pd.read_csv('../data/X_train.csv')
+y_train = pd.read_csv('../data/y_train.csv')
+X_test = pd.read_csv('../data/X_test.csv')
+y_test = pd.read_csv('../data/y_test.csv')
+
+# MLP 모델 설계
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(len(y_train['Obesity'].unique()), activation='softmax')  # 다중분류
+])
+
+# 컴파일
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# 학습
+history = model.fit(X_train, y_train, epochs=30, batch_size=32, validation_data=(X_test, y_test))
+
+# 결과 출력
+model.summary()
+test_loss, test_acc = model.evaluate(X_test, y_test)  # 테스트 데이터를 사용하여 모델 평가
+print("Test Accuracy:", test_acc)  # 테스트 정확도 출력
