b3603120145377eb31d3c6e4bb9f8ebfc184944e
Firebear518 <phj040518@gmail.com>
2025-02-24 14:49:59 +0900

    score test - LinearRegression

--- sleeptime.py
+++ sleeptime.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import tensorflow as tf
 
 df = pd.read_csv('datasets/sleeptime_prediction_dataset.csv')
 df = df.drop('ReadingTime', axis=1)
 df = df[(df['SleepTime'] <= 10) & (df['SleepTime'] >= 3)]
 
-feature_columns = ['WorkoutTime', 'PhoneTime', 'WorkHours', 'CaffeineIntake', 'RelaxationTime']
+feature = ['WorkoutTime', 'PhoneTime', 'WorkHours', 'CaffeineIntake', 'RelaxationTime']
+label = ['SleepTime']
 
 scaler = MinMaxScaler()
-df[feature_columns] = scaler.fit_transform(df[feature_columns])
+df[feature] = scaler.fit_transform(df[feature])
 
-X = df[feature_columns]
-y = df['SleepTime']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(df[feature], df[label], test_size=0.3, random_state=42)
 
-print(df.head())
+model = LinearRegression()
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
